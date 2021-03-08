e9b8f888ffeef0937f264ce8ef8edc6663cf5eb7
Stephane Simcic <simcic.stephane@gmail.com>
2021-03-09 00:28:11 +0800

    Adds username and image to Accounts table

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,14 +4,14 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(posts_params)
-    # if @post.save
-    #   flash[:success] = "Post successfully created"
-    #   redirect_to @post
-    # else
-    #   flash[:error] = "Something went wrong"
-    #   render 'new'
-    # end
+    @post = Post.new(post_params)
+    @post.account_id = current_account.id if account_signed_in?
+
+    if @post.save
+      redirect_to dashboard_path, flash: { success: "Post was created successfully!" }
+    else
+      redirect_to new_post_path, flash: { danger: "Post was not saved!" }
+    end
   end
     
   def show
@@ -19,7 +19,7 @@ class PostsController < ApplicationController
 
   private
 
-  def posts_params
+  def post_params
     params.require(:post).permit(:image, :image_cache)
   end
 end
